9485aff69ab2f5114f5cdd7d0b843e30719c82a3
Araliatrix <agentisaacson@gmail.com>
2018-07-03 10:24:49 -0600

    added library course component

---
+++ src/components/library/libraryCourse.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class LibraryCourse extends Component {
+	render() {
+		return (
+			<div className="library-course">
+                <label className="library-course__title">Problem Solving</label>
+                <div className='library-coursee__description'>
+                    <label>Course Description</label>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed gravida augue. Aliquam vel nulla augue. Phasellus venenatis porta posuere. Aenean rhoncus porta sapien. Suspendisse eget dolor sed orci ultricies aliquet non non arcu. Morbi auctor fermentum semper. Sed lacinia facilisis malesuada. Proin aliquam ex eget urna vulputate, vitae maximus massa facilisis. Aliquam ac diam sapien. Donec gravida leo elit, vel feugiat lacus cursus sed.</p>
+                </div>
+			</div>
+		);
+	}
+}
