abf546b951b8c5a7ec408130138cd9bd4e54deab
Araliatrix <agentisaacson@gmail.com>
2018-07-19 16:55:03 -0600

    styled addremove component

--- src/components/action.js
+++ src/components/action.js
@@ -2,12 +2,25 @@ import React, { Component } from "react";
 import { Icon } from "./icon";
 
 export default class Action extends Component {
+	handleAction = function() {
+		this.props.onClick();
+
+		
+	}.bind(this);
+
 	render() {
 		return (
-			<button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-				{Icon("fas fa-plus-circle")}
-				{/* {Icon("fas fa-times-circle")} */}
-			</button>
+			<a
+			id='action'
+				onClick={() => this.handleAction()}
+				className={`${this.props.className} action`}
+			/>
 		);
+		// return (
+		// 	<button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+		// 		{Icon("fas fa-plus-circle")}
+		// 		{/* {Icon("fas fa-times-circle")} */}
+		// 	</button>
+		// );
 	}
 }
